88fefbea43581a66efe8d9b3c78d9d75f416c52e
Abhigyan Prakash <72396135+Abhigyan2010@users.noreply.github.com>
2020-10-31 13:37:33 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,6 @@
+function sign_in()() {
+    P1_name=document.getElementById("P1-name").value;
+    P2_name=document.getElementById("P2-name").value;
+    localStorage.setItem("P1_name",P1name);
+    localStorage.setItem("P2_name",P2name);
+}
